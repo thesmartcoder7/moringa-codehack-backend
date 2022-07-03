63887c03f6a783492166ff85f890f5bcb095acf9
KakushaVenecia <kakushavenecia@gmail.com>
2022-07-03 13:16:53 +0300

    ft-Landing Page Routing: This will alter the landing page route

--- frontend/moringa-codehack/src/app/app-routing.module.ts
+++ frontend/moringa-codehack/src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'landing', pathMatch:'full'},
-  {path:'landing',component:LandingPageComponent},
+  {path:'',component:LandingPageComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
 
