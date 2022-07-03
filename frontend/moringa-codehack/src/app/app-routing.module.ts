9dce8c784f87a18378b8b16264ce60b9d34256b1
Machuka Joy <machukajoy@gmail.com>
2022-07-03 03:07:48 +0300

    ft: adds signup component, styling and routes

--- frontend/moringa-codehack/src/app/app-routing.module.ts
+++ frontend/moringa-codehack/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'landing', pathMatch:'full'},
   {path:'landing',component:LandingPageComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+
 ];
 
 @NgModule({
