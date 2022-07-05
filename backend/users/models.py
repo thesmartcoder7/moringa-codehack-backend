bba7600f1e182f3c6474feced294a28a12c21740
samuel martins <samuel.martins4.sm@gmail.com>
2022-07-06 00:21:57 +0300

    chore: clean migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
-
-# # Create your models here.
-# class User(AbstractUser):
-#     name = models.CharField(max_length=500)
-#     email = models.CharField(max_length=500, unique=True)
-#     password = models.CharField(max_length=500)
-#     username = None
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = []
-
-     
