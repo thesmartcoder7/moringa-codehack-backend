ab99bef46822b44f5753323eb6a545a336615058
samuel martins <samuel.martins4.sm@gmail.com>
2022-07-06 08:54:46 +0300

    feature: work on Angular JWT authentication

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=500)
     last_name = models.CharField(max_length=500)
-    username = models.CharField(max_length=500, unique=True, default='samuel')
+    username = models.CharField(max_length=500, unique=True, null=True)
     email = models.CharField(max_length=500)
     password = models.CharField(max_length=500)
    
