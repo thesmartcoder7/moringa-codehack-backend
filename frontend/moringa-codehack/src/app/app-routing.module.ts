58574bbf11c44bd4cc92b4113d6861ba41f299a7
KakushaVenecia <kakushavenecia@gmail.com>
2022-07-06 21:13:39 +0300

    chore - Changes to the assesment component

--- frontend/moringa-codehack/src/app/app-routing.module.ts
+++ frontend/moringa-codehack/src/app/app-routing.module.ts
@@ -6,16 +6,17 @@ import { SignupComponent } from './components/signup/signup.component';
 import{ StudentDashboardComponent } from './components/student-dashboard/student-dashboard.component';
 import { AssessmentComponent  } from './components/assessment/assessment.component';
 import { PractiseTestComponent } from './components/practise-test/practise-test.component';
+import { TakeTestComponent} from './components/take-test/take-test.component';
 // import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-  { path:'',component:LandingPageComponent},
-  { path:'login',component:LoginComponent},
-  { path:'signup',component:SignupComponent},
-  { path: 'student-dashboard', component: StudentDashboardComponent},
-  { path: 'assessment', component: AssessmentComponent},
-  { path: 'practise-test', component: PractiseTestComponent},
-
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'student-dashboard', component: StudentDashboardComponent },
+  { path: 'assessment', component: AssessmentComponent },
+  { path: 'practise-test', component: PractiseTestComponent },
+  { path: 'take-test', component: TakeTestComponent },
 ];
 
 @NgModule({
--- frontend/moringa-codehack/src/app/components/take-test/take-test.component.ts
+++ frontend/moringa-codehack/src/app/components/take-test/take-test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-take-test',
   templateUrl: './take-test.component.html',
