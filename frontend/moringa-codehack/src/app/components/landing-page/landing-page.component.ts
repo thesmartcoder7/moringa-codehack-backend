2d37286c70b8b42468cf7e987631a19844d73da3
samuel martins <samuel.martins4.sm@gmail.com>
2022-07-07 05:19:53 +0300

    chore: create a user class

--- frontend/moringa-codehack/src/app/components/landing-page/landing-page.component.ts
+++ frontend/moringa-codehack/src/app/components/landing-page/landing-page.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {Observable,of, from } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,28 +11,14 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
   message = 'Please login or signup';
+
   constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
-    //   this.http.get('http://localhost/8000/api/authenticated_user/').subscribe({
-    //     next: (v) => console.log(v),
-    //     error: (e) => console.error(e),
-    //     complete: () => console.info('complete')
-    // })
-    this.http
-      .get('http://localhost/8000/api/authenticated_user/', {
-        withCredentials: true,
-      })
-      .subscribe({
-        next: (res: any) => {
-          console.log(res);
-        },
-        error: (error: any) => {
-          console.log(error);
-        },
-        complete: () => {
-          console.log('complete');
-        },
-      });
+      this.http.get('http://localhost/8000/api/authenticated_user/').subscribe({
+        next: (v) => console.log(v),
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+    })
   }
 }
