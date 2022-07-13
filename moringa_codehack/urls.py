5c454cfc9e7deb9eeb176307427d906a1f7d5301
samuel martins <samuel.martins4.sm@gmail.com>
2022-07-14 01:09:24 +0300

    patch: configure heroku for release

--- moringa_codehack/urls.py
+++ moringa_codehack/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
